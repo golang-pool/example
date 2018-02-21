7366009c461b4aaf7014be8ffc8bf23c394acbd1
lijc08 <lijc08@vanke.com>
2018-02-22 00:52:15 +0800

    fix

--- exam1/container-rm.sh
+++ exam1/container-rm.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-imageName=exam1:1.1
-containerName=exam1
+containerName=$1
 
 if [ "$(docker ps -aq -f status=running -f name=${containerName})"  ]; then
 	docker stop ${containerName}
@@ -18,12 +17,3 @@ fi
 if [ "$(docker ps -aq -f status=dead -f name=${containerName})"  ]; then
 	docker rm -f ${containerName}
 fi
-
-if [ "$(docker images -q ${imageName})"  ]; then
-	docker rmi ${imageName}
-fi
-
-docker build -t ${imageName} .
-
-#docker run --rm -it --name ${containerName} ${imageName} ./main -v
-docker run --rm -it --name ${containerName} ${imageName} sh
