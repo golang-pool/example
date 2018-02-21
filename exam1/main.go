c3cbb775bcd3185684cc575e06afd63e51d9f8ab
lijc08 <lijc08@vanke.com>
2018-02-21 18:09:52 +0800

    fix:exam1

--- exam1/main.go
+++ exam1/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	"strings"
 )
 
 func noArgs(cmd *cobra.Command, args []string) error {
@@ -37,12 +38,24 @@ func main() {
 		},
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			// flags must be the top-level command flags, not cmd.Flags()
+			fmt.Println("PersistentPreRunE");
 			return nil
 		},
 	}
 	flags = cmd.Flags()
 	flags.BoolVarP(&Version, "version", "v", false, "Print version information and quit")
 
+	var cmdPrint = &cobra.Command{
+		Use:   "print [string to print]",
+		Short: "Print anything to the screen",
+		Long: `print is for printing anything back to the screen.
+For many years people have printed back to the screen.`,
+		Args: cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Print: " + strings.Join(args, " "))
+		},
+	}
+	cmd.AddCommand(cmdPrint)
 	if err := cmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
